cc0c9fda76c9307ccd31108aac024f1b25fd87cd
Mark Newton <marco@twistedpair.com.au>
2024-03-11 12:58:42 +1100

    New vimrc (trimmed down for remote ssh). Remove unused stuff

--- bin/install_fzf.sh
+++ bin/install_fzf.sh
@@ -3,6 +3,4 @@
 brew install fzf
 $(brew --prefix)/opt/fzf/install
 brew install ripgrep
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags
-brew install the_silver_searcher
 brew install fd
