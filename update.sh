af1f57da7dc57bb6ef0abc48035e325b934e1dd3
Mark Newton <marco@twistedpair.com.au>
2025-06-09 12:23:15 +1000

    fixes

--- update.sh
+++ update.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 HILITE="\e[38;5;34m"
 NC="\e[0m"
+NVIM_CONFIG=".config/nvim"
 
 cd "$(dirname "${BASH_SOURCE}")";
 
 # Check if .gitmodules exists and nvim submodule directory is empty or missing
 if [ -f ".gitmodules" ]; then
-  if [ ! -d ".config/nvim/.git" ]; then
+  if [ ! -f "$NVIM_CONFIG/.git" ]; then
     printf "Initialising submodules...\n"
     git submodule update --init --recursive
   fi
 fi
 
 git pull origin master
+if [ -f "$NVIM_CONFIG/.git" ]; then
 git submodule update --remote --merge
+elif [ -d "$NVIM_CONFIG/.git" ]; then
+cd $NVIM_CONFIG
+git pull origin master
+fi
+if [ -d "$HOME/$NVIM_CONFIG/.git" ]; then
+cd $HOME/$NVIM_CONFIG
+git pull origin master
+fixperms
+fi
 printf '%-40s' "Git pulled latest:"
 printf "${HILITE}OK${NC}\n"
+cd "$(dirname "${BASH_SOURCE}")";
 fixperms
 printf '%-40s' "Fixed permissions:"
 printf "${HILITE}OK${NC}\n"
